fa5fe8a4cafeea0e7c0e62831abd62467229f96b
Aman <122103691+amanraj078@users.noreply.github.com>
2024-11-29 16:30:46 +0530

    first commit

--- client/src/pages/checkout.jsx
+++ client/src/pages/checkout.jsx
@@ -7,44 +7,46 @@ import { Elements } from "@stripe/react-stripe-js";
 import CheckoutForm from "../components/CheckoutForm";
 import { useRouter } from "next/router";
 
-const stripePromise = loadStripe("pk_test_xeqIPdYS2PpKbHmKG4gJqpde");
+const stripePromise = loadStripe(
+    "pk_test_51QQROKAyJr5FjGZiYtgf5EiorgsG22MpcQl2WUjruPO1iRHeTq2uJAt61mNNfUZqAYgICYSUU46Fp9JZJaF4cOfC0025N7XkHs"
+);
 
 function Checkout() {
-  const [clientSecret, setClientSecret] = useState("");
-  const router = useRouter();
-  const { gigId } = router.query;
-  useEffect(() => {
-    const createOrderIntent = async () => {
-      const { data } = await axios.post(
-        CREATE_ORDER,
-        { gigId },
-        { withCredentials: true }
-      );
-      setClientSecret(data.clientSecret);
+    const [clientSecret, setClientSecret] = useState("");
+    const router = useRouter();
+    const { gigId } = router.query;
+    useEffect(() => {
+        const createOrderIntent = async () => {
+            const { data } = await axios.post(
+                CREATE_ORDER,
+                { gigId },
+                { withCredentials: true }
+            );
+            setClientSecret(data.clientSecret);
+        };
+        if (gigId) createOrderIntent();
+    }, [gigId]);
+
+    const appearance = {
+        theme: "stripe",
     };
-    if (gigId) createOrderIntent();
-  }, [gigId]);
+    const options = {
+        clientSecret,
+        appearance,
+    };
 
-  const appearance = {
-    theme: "stripe",
-  };
-  const options = {
-    clientSecret,
-    appearance,
-  };
-
-  return (
-    <div className="min-h-[80vh] max-w-full mx-20 flex flex-col gap-10 items-center">
-      <h1 className="text-3xl">
-        Please complete the payment to place the order.
-      </h1>
-      {clientSecret && (
-        <Elements options={options} stripe={stripePromise}>
-          <CheckoutForm />
-        </Elements>
-      )}
-    </div>
-  );
+    return (
+        <div className="min-h-[80vh] max-w-full mx-20 flex flex-col gap-10 items-center">
+            <h1 className="text-3xl">
+                Please complete the payment to place the order.
+            </h1>
+            {clientSecret && (
+                <Elements options={options} stripe={stripePromise}>
+                    <CheckoutForm />
+                </Elements>
+            )}
+        </div>
+    );
 }
 
 export default Checkout;
